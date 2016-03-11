73161b1a7a2492ca439e9bd81bba5a7592e42f4a
Matt Gardner <wilburnforce@gmail.com>
2016-03-11 18:02:57 -0500

    Initial commit.

---
+++ charts/CostBurdenSubregion.rb
@@ -0,0 +1,9 @@
+def transpose(data, opts):
+
+end
+
+
+Chart.new(
+  table: b25091_b25070_costburden_acs_m,
+  munge: transpose
+).generate
---
+++ generate.rb
@@ -0,0 +1,55 @@
+require 'axlsx'
+require 'pg'
+
+# Housing Charts
+class Munger
+  def initialize(args)
+
+  end
+end
+
+class Workbook
+  attr_reader :excel
+
+  def initialize 
+    @excel = Axlsx::Package.new
+  end
+
+  def workbook
+    @excel.workbook
+  end
+
+  def some_other_stuff
+
+  end
+end
+
+class Batch
+  attr_reader :workbook
+
+  def initialize(workbook)
+    @workbook = workbook
+  end
+
+  def connection
+    PG::Connection.open(dbname: 'ds', host: '10.10.10.240', user:'dsviewer', password:'dsview933')
+  end
+end
+
+class Chart
+  attr_reader :batch
+
+  def initialize(batch)
+    @batch = batch
+  end
+
+  def generate
+    @batch.workbook.workbook.add_worksheet(name: "TestAsdf") do |sheet|
+
+    end
+  end
+end
+
+workbook = Workbook.new
+batch = Batch.new(workbook)
+chart = Chart.new(batch)
